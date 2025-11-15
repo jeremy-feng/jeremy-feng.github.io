9cd6d579520a2832effeb9060b6312e18dcc0072
jeremy-feng <jeremy-feng@qq.com>
2025-11-16 01:52:19 +0800

    docs: update

--- javascripts/wechat-link.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var el = document.querySelector('.md-social__link[title="WeChat"]');
-  if (el) {
-    el.addEventListener('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-    });
-  }
-});
